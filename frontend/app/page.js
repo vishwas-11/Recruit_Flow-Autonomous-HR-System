d84184117df342be95c9f86f9ebeafe3ac551ec2
Vishwas Charan <vishwascharan11@gmail.com>
2026-04-08 17:15:23 +0530

    removed unnecessary lines of code

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,35 +1,3 @@
-// "use client";
-// import { useRouter } from "next/navigation";
-
-// export default function Home() {
-//   const router = useRouter();
-
-//   return (
-//     <div className="h-screen flex flex-col justify-center items-center bg-gray-900 text-white">
-//       <h1 className="text-4xl font-bold mb-6">
-//         RecruitFlow 🚀
-//       </h1>
-
-//       <p className="mb-6 text-gray-300">
-//         AI Powered Hiring & Onboarding System
-//       </p>
-
-//       <button
-//         onClick={() => router.push("/auth")}
-//         className="bg-blue-500 px-6 py-3 rounded-lg hover:bg-blue-600"
-//       >
-//         Get Started
-//       </button>
-//     </div>
-//   );
-// }
-
-
-
-
-
-
-
 "use client";
 import { useRouter } from "next/navigation";
 import Navbar from "@/components/Navbar";
