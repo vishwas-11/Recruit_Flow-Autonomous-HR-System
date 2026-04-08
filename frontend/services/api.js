efe7235a4ce9b96f1599f282e78d81349b1b08e8
Vishwas Charan <vishwascharan11@gmail.com>
2026-04-08 16:35:38 +0530

    cleaned codebase

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,22 +1,3 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "http://127.0.0.1:8000",
-// });
-
-// // Attach token automatically
-// API.interceptors.request.use((req) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     req.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return req;
-// });
-
-// export default API;
-
-
-
 import axios from "axios";
 
 const API = axios.create({
