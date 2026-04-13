c95cf06ea6c04e11142344f7bfb418900e683e1f
Vishwas Charan <vishwascharan11@gmail.com>
2026-04-13 13:26:34 +0530

    fixed cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,14 +10,10 @@ load_dotenv()
 app = FastAPI()
 
 
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],   # allow all methods (GET, POST, OPTIONS)
     allow_headers=["*"],
